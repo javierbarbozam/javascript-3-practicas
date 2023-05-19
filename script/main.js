87f5d08acf81d114fdb1b39a1db9a0a690e588cd
javierbarbozam <barboza.javier497@gmail.com>
2023-05-19 00:20:47 -0600

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,5 +1,5 @@
 import generateRandomJoke from "./modules/RandomJoke.js";
-import jokesFound from "./modules/SearchJoke.js";
+import SearchJokeBtn from "./modules/SearchJoke.js";
 
-generateRandomJoke
-jokesFound
+generateRandomJoke()
+SearchJokeBtn()
