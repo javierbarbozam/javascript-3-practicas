51aaf7188c57338a8a016b1d40395e0c857004ed
javierbarbozam <barboza.javier497@gmail.com>
2023-06-20 01:51:42 -0600

    add styles

--- src/components/event/components/showEvent.js
+++ src/components/event/components/showEvent.js
@@ -11,19 +11,19 @@ const showEvents = async (value) => {
   let eventList = "";
   events.forEach((element) => {
     eventList += `<li class="event-item">
-      <button data-id="js-favorite-btn" data-event_id="${element.id}" data-state="favorite">
-        <span class="material-symbols-rounded">favorite</span>
-      </button> 
       <img src="${element.image}" alt="" class="event-item__img">
       <div class="event-item__wrapper">
         <p class="event-item__title">${element.title}</p>
         <p class="event-item__date">${getDate(element.date)}</p>
         <p class="event-item__location">${element.location.address} • ${element.location.city}, ${element.location.state}</p>
         <span class="event-item__price">${getPrice(element.price)}</span>
-        <form>
-        <button data-id="event_state" data-event_id="${element.id}" value="interested">Interested</button>
-        <button data-id="event_state" data-event_id="${element.id}" value="going">Going</button>
-        </form>
+        <div class="event-item__btn-wrapper">
+          <button class="event-item__btn event-item__btn--favorite" data-id="js-favorite-btn" data-event_id="${element.id}" data-state="favorite">
+            <span class="material-symbols-rounded">favorite</span>
+          </button> 
+          <button class="event-item__btn event-item__btn--state" data-id="event_state" data-event_id="${element.id}" value="interested">Interested</button>
+          <button class="event-item__btn event-item__btn--state" data-id="event_state" data-event_id="${element.id}" value="going">Going!</button>
+        </div>
       </div>
     </li>`;
   });
