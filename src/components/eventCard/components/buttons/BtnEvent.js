73d0465c7d71809c5c56dfcb27aeeb7d61f03615
javierbarbozam <barboza.javier497@gmail.com>
2023-07-07 02:36:59 -0600

    fix(buttons.js) fix structure

--- src/components/eventCard/components/buttons/BtnEvent.js
+++ src/components/eventCard/components/buttons/BtnEvent.js
@@ -1,41 +1,58 @@
 import { cacheProxy } from "../../../cache/cache.js";
+import { stateImmutable } from "../../../state/state.js";
+import { getCategory } from "../getCategory.js";
 
-const filterEvent = (category, element) => {
+const eventFromCache = (category, element) => {
   const eventId = element.dataset.event_id;
-  const currentCategory = category;
-  const events = cacheProxy[currentCategory];
+  const events = cacheProxy[category];
   const result = events.find((element) => element.id === eventId);
   return result;
 };
 
-const handleFavorite = (event, category, button) => {
+const handleState = (category, button) => {
+  // Get specific state array
+  const stateCategory = button.value;
   const state = stateImmutable.getState();
-  const { favorite } = state;
-  const result = favorite.find((element) => element.id === event.id);
-  if (!result) {
-    stateImmutable.addEvent(category, event);
-    handleFavoriteStyles(button, true);
+
+  // const key = state[stateCategory];
+  // const filterEvent = key.find(element => element.id === currentEvent.id)
+
+  // Get clicked event data
+  const currentEvent = eventFromCache(category, button);
+
+  // Filter event from state
+  const filterEvent = stateImmutable.getEvent(stateCategory, currentEvent.id);
+
+  // Add or Remove from state
+  if (filterEvent) {
+    stateImmutable.removeEvent(stateCategory, currentEvent);
   } else {
-    stateImmutable.removeEvent(category, event);
-    handleFavoriteStyles(button);
+    if (stateCategory === "favorite") {
+      stateImmutable.addEvent(stateCategory, currentEvent);
+    } else if (stateCategory === "going") {
+      stateImmutable.addEvent(stateCategory, currentEvent);
+      stateImmutable.removeEvent("interested", currentEvent);
+    } else {
+      stateImmutable.addEvent(stateCategory, currentEvent);
+      stateImmutable.removeEvent("going", currentEvent);
+    }
   }
 };
 
-const addBtnEvent = (tabCategory) => {
-  const currentCategory = tabCategory;
+const addButtonEvent = (category) => {
+  const tabCategory = category;
+  const events = cacheProxy[tabCategory];
   const buttons = document.querySelectorAll(".event-item__btn");
+
   buttons.forEach((element) =>
     element.addEventListener("click", () => {
-      const currentEvent = filterEvent(currentCategory, element);
-      console.log(currentEvent);
-      // element.value, element.dataset.event_id
-      if (element.value === "favorite") {
-        // agregar lógica
-      } else if (element.value === "going") {
-      } else {
-      }
+      // Get needed data from button
+      const eventId = element.dataset.event_id;
+      const stateCategory = element.value;
+
+      handleState(tabCategory, element);
     })
   );
 };
 
-export { addBtnEvent };
+export { addButtonEvent };
--- src/components/eventCard/initEvents.js
+++ src/components/eventCard/initEvents.js
@@ -1,19 +1,13 @@
 import { showEvents } from "./components/showEvent.js";
-import { handleFavoriteEvent } from "../eventCard/components/buttons/addFavorites.js";
-import { handleInterestedEvent } from "../state/addInterested.js";
 import { getCategory } from "./components/getCategory.js";
 import { getEventCategory } from "./components/getEvent.js";
-import { addBtnEvent } from "./components/buttons/BtnEvent.js";
+import { addButtonEvent } from "./components/buttons/BtnEvent.js";
 
 const initEvents = async () => {
   const category = getCategory();
   const events = await getEventCategory(category);
   showEvents(events);
-  addBtnEvent(category);
-  // add favorites event listener
-  // handleFavoriteEvent();
-  // add favorites event listener
-  // handleInterestedEvent();
+  addButtonEvent(category);
 };
 
 export { initEvents };
