32645dba57caf69e7318e680e50d0e96596d6d29
javierbarbozam <barboza.javier497@gmail.com>
2023-07-08 00:37:07 -0600

    add text to button

--- src/components/eventCard/components/buttons/btnStyleHandler.js
+++ src/components/eventCard/components/buttons/btnStyleHandler.js
@@ -1,21 +1,38 @@
 const handleBtnStyle = (category, element) => {
-  const validation = element.classList.contains("event-item__btn--active");
+  const activeClass = element.classList.contains("event-item__btn--active");
 
   switch (category) {
     case "going":
-      const interestedBtn = document.querySelector(`button[value="interested"][id="${element.id}"]`);
-      interestedBtn.style.display = validation ? "block" : "none";
-      element.innerHTML = validation ? category : "Changed your mind?";
       element.classList.toggle("event-item__btn--active");
+      const interestedBtn = document.querySelector(`button[value="interested"][id="${element.id}"]`);
+      interestedBtn.style.display = activeClass ? "block" : "none";
+      element.innerHTML = activeClass ? category : `You are ${category}. Changed your mind?`;
       break;
     case "interested":
-      element.innerHTML = validation ? category : "Changed your mind?";
       element.classList.toggle("event-item__btn--active");
+      element.innerHTML = activeClass ? category : `You are ${category}. Changed your mind?`;
       break;
     default:
       element.classList.toggle("event-item__btn--favorite--active");
       break;
   }
 };
+
+function findObjectById(id) {
+  let foundObject = null;
+  let foundArray = null;
+
+  for (let key in stateImmutable) {
+    if (Array.isArray(stateImmutable[key])) {
+      foundObject = stateImmutable[key].find((obj) => obj.id === id);
+      if (foundObject) {
+        foundArray = key;
+        break;
+      }
+    }
+  }
+
+  return { element: foundObject, array: foundArray };
+}
 
 export { handleBtnStyle };
