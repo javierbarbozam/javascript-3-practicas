e69b48f01e065cfdf97cadf33f2ed9bde3c4cd3c
javierbarbozam <barboza.javier497@gmail.com>
2023-07-10 00:36:47 -0600

    Fix(tab) delete extra tab bug

--- src/components/tabs/components/createTabs.js
+++ src/components/tabs/components/createTabs.js
@@ -2,7 +2,6 @@ import { accountTab } from "./accountTab.js";
 
 const createTabs = (value) => {
   const container = document.getElementById("category-nav");
-  const account = accountTab();
   let { tab, path } = accountTab();
   value.forEach( (element) =>
     (tab +=
@@ -12,7 +11,6 @@ const createTabs = (value) => {
         href="${path}?category=${element.category}">${element.category}</a>
       </li>`)
   );
-  tab += account;
   container.innerHTML = tab;
 };
 
