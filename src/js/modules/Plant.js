a7d002ef2647605c430a7ec0104d919bb552a7c7
javierbarbozam <barboza.javier497@gmail.com>
2023-07-17 16:59:44 -0600

    create plant builder

---
+++ src/js/modules/Plant.js
@@ -0,0 +1,44 @@
+class Plant {
+  lightQuantity(value) {
+    this.light = value;
+    return this;
+  }
+
+  soilType(value) {
+    this.soil = value;
+    return this;
+  }
+
+  withPets() {
+    this.isToxic = false;
+    return this;
+  }
+
+  noPets() {
+    this.isToxic = true;
+    return this;
+  }
+
+  ceramicPot() {
+    this.potMaterial = 'ceramic';
+    return this;
+  }
+
+  clayPot() {
+    this.potMaterial = 'clay';
+    this.soil = 'drainage';
+    return this;
+  }
+
+  potStyle(value) {
+    this.potDecoration = value;
+    return this;
+  }
+
+  addExtras(value) {
+    this.extras = value;
+    return this;
+  }
+}
+
+export { Plant };
