694ea17941111b58f1e02e8c6a3bd05195599014
javierbarbozam <barboza.javier497@gmail.com>
2023-07-17 22:28:22 -0600

    Create plant-config.js

---
+++ src/js/plant-config.js
@@ -0,0 +1,34 @@
+const plantConfig = {
+  low_light: {
+    toxic: {
+      default: 'sansevieria',
+      overwater: 'peace lily',
+    },
+    non_toxic: {
+      default: 'boston fern',
+      overwater: 'boston fern',
+    },
+  },
+  medium_light: {
+    toxic: {
+      default: 'aglaonema',
+      overwater: 'peace lily',
+    },
+    non_toxic: {
+      default: 'monstera',
+      overwater: 'peace lily',
+    },
+  },
+  outdoor: {
+    toxic: {
+      default: 'aloe vera',
+      overwater: 'aloe vera',
+    },
+    non_toxic: {
+      default: 'cactus',
+      overwater: 'cactus',
+    },
+  },
+};
+
+export { plantConfig };
