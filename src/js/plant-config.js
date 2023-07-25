b5421a0fdcbf3de8c6ab338d1bcb0ca69d7e1990
javierbarbozam <barboza.javier497@gmail.com>
2023-07-24 20:13:17 -0600

    Fix images name

--- src/js/plant-config.js
+++ src/js/plant-config.js
@@ -2,27 +2,27 @@ const plantConfig = {
   low_light: {
     toxic: {
       default: 'sansevieria',
-      overwater: 'peace lily',
+      overwater: 'peace-lily',
     },
     non_toxic: {
-      default: 'boston fern',
-      overwater: 'boston fern',
+      default: 'boston-fern',
+      overwater: 'boston-fern',
     },
   },
   medium_light: {
     toxic: {
       default: 'aglaonema',
-      overwater: 'peace lily',
+      overwater: 'peace-lily',
     },
     non_toxic: {
       default: 'monstera',
-      overwater: 'peace lily',
+      overwater: 'peace-lily',
     },
   },
   outdoor: {
     toxic: {
-      default: 'aloe vera',
-      overwater: 'aloe vera',
+      default: 'aloe-vera',
+      overwater: 'aloe-vera',
     },
     non_toxic: {
       default: 'cactus',
