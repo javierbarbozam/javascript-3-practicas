e5de1e721c4a4b4559e8ed598b90b2582c4b82db
javierbarbozam <barboza.javier497@gmail.com>
2023-08-10 18:05:38 -0600

    index without styles and index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { renderPlant } from './js/modules/renderPlant.js';
 import { userPlant, PlantData } from './js/modules/getPlant.js';
 import { initCustomizeForm } from './js/customize-form.js';
-// import './styles/styles.css';
+import './styles/styles.css';
 
 const initPlant = async () => {
   await PlantData();
